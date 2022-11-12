a1320fa88d41e852c1fa53ed03e9a10a3b80a04c
elxcrack3r <elxcracker@outlook.com>
2022-11-12 07:24:13 -0500

    defining the target

--- mapp1ng.py
+++ mapp1ng.py
@@ -2,3 +2,12 @@ import queue
 import threading
 import os
 import urllib
+
+threads = 10
+
+target = "https://www.newforum.eu"
+directory = "/usr/share/wordlists/common.txt"
+filters = [".jpg", ".gif", ".png", ".css"]
+
+os.chdir(directory)
+
