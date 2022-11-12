a51f85ce5d26fcf9e27b87b91b976ea16e7ff5e4
elxcrack3r <elxcracker@outlook.com>
2022-11-12 07:43:26 -0500

    Ending up changes for version alpha

--- mapp1ng.py
+++ mapp1ng.py
@@ -5,8 +5,8 @@ import urllib
 
 threads = 10
 
-target = "https://www.newforum.eu"
-directory = "/usr/share/wordlists/common.txt"
+target = "https://www.backhatpython.com"
+directory = "/usr/share/wordlists/dirb/common.txt"
 filters = [".jpg", ".gif", ".png", ".css"]
 
 os.chdir(directory)
@@ -21,3 +21,23 @@ for r,d,f in os.walk("."):
         if os.path.splitext(files)[1] not in filters:
             web_paths.put(remote_path)
 
+def test_remote():
+    while not web_paths.empty():
+        path = web_paths.get()
+        url = "%s%s" % (target, path)
+
+        request = urllib.Request(url)
+        try:
+            response = urllib.urlopen(request)
+            content = response.read()
+
+            print ("[%d] => %s") % (response.code, path)
+            response.close()
+
+        except urllib.HTTPError as error:
+            #print "Failed %s" % error.code
+            pass
+for i in range(threads):
+    print ("Spawning thread: %d") % i
+    t = threading.Thread(target=test_remote)
+    t.start()
