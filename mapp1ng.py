502cdfcddad6389a40f78872a8e2f0244d0062ef
elxcrack3r <elxcracker@outlook.com>
2022-11-12 07:29:23 -0500

    working on the basics

--- mapp1ng.py
+++ mapp1ng.py
@@ -11,3 +11,13 @@ filters = [".jpg", ".gif", ".png", ".css"]
 
 os.chdir(directory)
 
+web_paths = queue.Queue()
+
+for r,d,f in os.walk("."):
+    for files in f:
+        remote_path = "%s/%s" % (r,files)
+        if remote_path.startswith("."):
+            remote_path = remote_path[1:]
+        if os.path.splitext(files)[1] not in filters:
+            web_paths.put(remote_path)
+
